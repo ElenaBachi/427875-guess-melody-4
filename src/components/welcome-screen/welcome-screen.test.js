94ec66a74ea1786e5590f44aaff2f43ff3cfc196
ElenaBachi <bachi.elena@gmail.com>
2020-06-14 10:46:27 +0300

    Автоматизированное тестирование с jest и Enzyme

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import WelcomeScreen from "./welcome-screen.jsx";
+
+describe(`Render WelcomeScreen`, () => {
+  it(`Should WelcomeScreen render correctly`, () => {
+    const tree = renderer
+      .create(<WelcomeScreen
+        errorsCount={3}
+        onWelcomeButtonClick={() => {}}
+      />)
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
